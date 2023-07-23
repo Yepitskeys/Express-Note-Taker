023c3a7bc867b522bec9424395b95d6ce192d0df
Keiquan Blackmon <120697653+Yepitskeys@users.noreply.github.com>
2023-07-23 19:29:31 -0400

    added uuid package and updated routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const store = require("../db/db.json");
+let notes = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        notes.push(newNote);
+
+        let stringNotes = JSON.stringify(notes, null, 3);
+
+        fs.writeFile(`./db/db.json`, stringNotes, (err) =>
+        err
+            ? console.error(err)
+            : console.log(`Note was added!`) 
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } 
+});
+
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 
 // Creates the port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Creates the express app
 const app = express();
 
+// Creates routes
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 // Handle parsing of data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Creates the public folder host
 app.use(express.static('public'));
 
